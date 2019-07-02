a7e98a475cd858c465975dd15af631fc74cb4c38
johnvardanyan99 <john.vardanyan99@gmail.com>
2019-07-02 12:33:33 +0400

    first commit

---
+++ js/Script.js
@@ -0,0 +1,15 @@
+const odd = document.querySelectorAll('.odd')
+const even = document.querySelectorAll('.even')
+
+const foo = () => {
+    for (let i = 0; i < odd.length; i++) {
+        odd[i].style.left = '90%'
+        odd[i].style.transition = '5s'
+    }
+    
+    for (let j = 0; j < even.length; j++) {
+        even[j].style.right = '90%'
+        even[j].style.transition = '5s'
+    }
+    
+}
